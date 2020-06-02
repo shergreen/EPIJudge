a3a398cc509617a2cbd3cdbafec7068e6179f4ad
Sheridan Beckwith Green <sheridan.green@yale.edu>
2020-06-02 18:16:48 -0400

    solved h-index python problem

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def h_index(citations: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    citations.sort()
+    total_papers = len(citations)
+    for i in range(0, total_papers):
+        if(citations[i] >= total_papers - i):
+            h = total_papers - i
+            return h
+    return 0 # only the case if all papers have no citations
 
 
 if __name__ == '__main__':
